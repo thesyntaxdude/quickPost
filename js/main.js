5c85ca510084a42796a33a0cdf3efb9eeed49f9c
thesyntaxdude <prince.akakpo@icloud.com>
2025-06-17 12:53:40 +0000

    JS: Implemented smooth navigation through auth popups'

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// LOGINS "ANIMATION" - SMOOTH TRANSITION THROUGH POPUPS
+const signupBtn = document.querySelector("#signup-btn");
+const loginBtn = document.querySelector("#login-btn");
+const alreadySignedUp = document.querySelector(".login");
+const noAccountYet = document.querySelector(".signup");
+
+signupBtn.addEventListener("click", () => {
+  const signupForm = document.querySelector("#signup-form");
+  signupForm.classList.toggle("active");
+});
+loginBtn.addEventListener("click", () => {
+  const loginForm = document.querySelector("#login-form");
+  loginForm.classList.toggle("active");
+});
+alreadySignedUp.addEventListener("click", () => {
+  const loginForm = document.querySelector("#login-form");
+  const signupForm = document.querySelector("#signup-form");
+  loginForm.classList.toggle("active");
+  signupForm.classList.remove("active");
+});
+noAccountYet.addEventListener("click", () => {
+  const signupForm = document.querySelector("#signup-form");
+  const loginForm = document.querySelector("#login-form");
+  signupForm.classList.toggle("active");
+  loginForm.classList.remove("active");
+});
